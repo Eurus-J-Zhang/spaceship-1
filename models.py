5b902fbb1676925fa59e9cabcd9ea2929888093d
Eurus J Zhang <zhangjy@jyu.fi>
2025-01-15 12:07:29 +0200

    hi

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Data(db.Model):
+    number = db.Column(db.Integer, primary_key=True)
+    id= db.Column(db.String(20))
+    gender=db.Column(db.String(1))
+    age = db.Column(db.Integer)
+
+    final_choice = db.Column(db.String(1))
+
+    despair = db.Column(db.Integer)
+    anxiety = db.Column(db.Integer)
+    irritation = db.Column(db.Integer)
+    rage = db.Column(db.Integer)
+    shame = db.Column(db.Integer)
+    guilt = db.Column(db.Integer)
+
+
+    feedback = db.Column(db.String(600))
